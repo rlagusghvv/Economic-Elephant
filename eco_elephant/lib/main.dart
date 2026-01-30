fa58fdf9384279917252437f82692a2b98aac466
rlagusghvv <eue5003@naver.com>
2026-01-30 17:52:15 +0900

    fix: add ScrollDirection import

--- eco_elephant/lib/main.dart
+++ eco_elephant/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
